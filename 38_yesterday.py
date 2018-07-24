5c409d4abe89bd1214dddebc98eb2c5e1686c26e
Jongpil Choi <jongpil.choi@gmail.com>
2018-07-23 17:20:08 -0700

    7/23 update

---
+++ 38_yesterday.py
@@ -0,0 +1,12 @@
+f = open("yesterday.txt", "r")
+yesterday_lyric = ""
+while 1:
+	line = f.readline()	
+	if not line:
+		break
+	yesterday_lyric = yesterday_lyric + line.strip() + "\n"
+f.close()
+print(yesterday_lyric)
+n_of_yesterday = yesterday_lyric.upper().count("YESTERDAY")
+print("Yesterday number: ", n_of_yesterday)
+
